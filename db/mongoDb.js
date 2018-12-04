ce12aab3d2544fc3d908a0d0d702544f7b36ee3e
Jaime Lopez <detec.azul@gmail.com>
2018-12-04 09:53:06 +0000

    User route no longer needed. Server will be in charge of checking for users on non-auth endpoints

--- db/mongoDb.js
+++ db/mongoDb.js
@@ -5,7 +5,9 @@ module.exports = function(app) {
 	MongoClient.connect(MONGO_URI, { useNewUrlParser: true })
 		.then(async client => {
 			const db = client.db('precaria');
-			app.users = db.collection('users');
+			app.users = db.collection('admins');
+			app.tickets = db.collection('tickets');
+			app.customers = db.collection('customers');
 			console.log('Database connection established');
 		})
 		.catch(err => console.error(err));
--- router/index.js
+++ router/index.js
@@ -3,20 +3,21 @@ const router = new Router();
 const app = require('../server.js');
 
 //Importing routers
-const userRouter = require('./secureEndPoints/userRouter.js');
+// const userRouter = require('./secureEndPoints/userRouter.js');
 
 //Connection to DB
 require('../db/mongoDb.js')(app);
 
 //Index Routes
-router.get('/api/v2', async ctx => {
-	ctx.body = {
-		status: 'success',
-		message: 'hello, world!'
-	};
-});
+
+// router.get('/api/v2', async ctx => {
+// 	ctx.body = {
+// 		status: 'success',
+// 		message: 'hello, world!'
+// 	};
+// });
 
 //Re-routing
-router.use(userRouter.routes());
+// router.use(userRouter.routes());
 
 module.exports = router;
